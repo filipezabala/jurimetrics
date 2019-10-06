03d50a598f7d819e90be52de14c62aec065182c7
Filipe Zabala <filipezabala@Filipes-MacBook-Air.local>
2019-10-06 15:05:06 -0300

    legend position

--- R/theme_doj.R
+++ R/theme_doj.R
@@ -15,6 +15,7 @@
 theme_doj <- function(){
   return(
     theme(
+      legend.position = 'bottom',
       panel.background = element_rect(fill = 'grey50', colour = NA),
       plot.background = element_rect(fill = 'transparent', color = NA),
       strip.background = element_rect(fill = 'transparent', colour = NA),
@@ -25,8 +26,7 @@ theme_doj <- function(){
       legend.text = element_text(color='grey42'),
       panel.grid = element_line(colour = 'grey42'),
       panel.grid.major = element_line(size = rel(0.5)),
-      panel.grid.minor = element_line(size = rel(0.5),
-      legend.position = 'bottom')
+      panel.grid.minor = element_line(size = rel(0.5))
       )
   )
 }

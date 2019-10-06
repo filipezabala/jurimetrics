130784457aa6ee9fba47e941ee736fd5627a9952
Filipe Zabala <filipezabala@Filipes-MacBook-Air.local>
2019-10-06 14:59:38 -0300

    legend bottom

--- R/theme_doj.R
+++ R/theme_doj.R
@@ -25,7 +25,8 @@ theme_doj <- function(){
       legend.text = element_text(color='grey42'),
       panel.grid = element_line(colour = 'grey42'),
       panel.grid.major = element_line(size = rel(0.5)),
-      panel.grid.minor = element_line(size = rel(0.5))
+      panel.grid.minor = element_line(size = rel(0.5),
+      legend.position = 'bottom')
       )
   )
 }
